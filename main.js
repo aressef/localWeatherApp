713e2a87f9e0d4340835f1538dce7487daf40ca3
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-05-13 13:30:12 -0400

    added ability to toggle between fahrenheit and celsius. Also restructured main.js functions into methods inside an object

--- main.js
+++ main.js
@@ -1,53 +1,57 @@
-// js file
-
-// Get location
-
-
 var city = document.querySelectorAll('.city');
 var temp = document.querySelectorAll('.temp');
 var weather = document.querySelectorAll('.weather');
 
-city.innerHTML = 'hello';
+// Get location
+var functions = {
+  getLocation: function() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.showPosition);
+    }
+  },
+  showPosition: function(position) {
+    // x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browswer.";
+    var pathWithCurrentLocal = 'https://weather.millergeek.xyz/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=31405be1fb8f6e7f0bb74137ccb08125';
+
+    ajaxRequest(pathWithCurrentLocal, function(data){
+      var getTemp = data.main.temp;
+      var getCity = data.name;
+      var getWeather = data.weather[0].description;
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+
+      // Converting Weather from Kelvin to Fahrenheit and Celsius
+      var fahrenheitConverter = Math.round((getTemp * (9/5) - 459.67) * 100) / 100;
+      var celsiusConverter = Math.round((getTemp - 273.15) * 100) / 100;
+      fahrenheit = fahrenheitConverter + " fahrenheit";
+      celsius = celsiusConverter + " celsius";
+
+
+
+      city[0].innerHTML = getCity;
+      temp[0].innerHTML = fahrenheit;
+      weather[0].innerHTML = getWeather;
+
+      console.log(data);
+      console.log(getTemp);
+      console.log(getCity);
+      console.log(getWeather);
+      console.log("Lat: " + lat);
+      console.log("Lon: " + lon);
+    });
+  },
+  toggleTemp: function() {
+    var currentTempType = document.getElementById('temp');
+    if (currentTempType.innerHTML == fahrenheit) {
+      currentTempType.innerHTML = celsius;
+    } else {
+      currentTempType.innerHTML = fahrenheit;
+    }
   }
-}
-
-function showPosition(position) {
-  // x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-  var pathWithCurrentLocal = 'https://weather.millergeek.xyz/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&APPID=31405be1fb8f6e7f0bb74137ccb08125';
-
-  ajaxRequest(pathWithCurrentLocal, function(data){
-    var getTemp = data.main.temp;
-    var getCity = data.name;
-    var getWeather = data.weather[0].description;
-    var lat = data.coord.lat;
-    var lon = data.coord.lon;
-
-    // Converting Weather to Fahrenheit from Kelvin
-
-    var fahrenheit = Math.round((getTemp * (9/5) - 459.67) * 100) / 100;
-    var celcius = Math.round((getTemp - 273.15) * 100) / 100;
-
-
-
-    city[0].innerHTML = getCity;
-    temp[0].innerHTML = "Fahrenheit : " + fahrenheit + "<br>" +  "Celcius: " + celcius;
-    weather[0].innerHTML = getWeather;
-
-    console.log(data);
-    console.log(getTemp);
-    console.log(getCity);
-    console.log(getWeather);
-    console.log("Lat: " + lat);
-    console.log("Lon: " + lon);
-  });
-
-}
+};
 
 
 // AJAX request
@@ -70,7 +74,7 @@ function ajaxRequest (path, callback) {
   xhr.send(null);
 }
 
-getLocation();
+functions.getLocation();
 
 
 
