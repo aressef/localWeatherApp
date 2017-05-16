b2d12fe2b4dbaf12b86dd3593764d3ea9761612d
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-05-16 18:37:21 -0400

    added getWeatherIcon function to main.js which displays an image accoring to weather.innerHTML. Also added img folder for weather images.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var city = document.querySelectorAll('.city');
-var temp = document.querySelectorAll('.temp');
-var weather = document.querySelectorAll('.weather');
+var city = document.getElementById('city');
+var temp = document.getElementById('temp');
+var weather = document.getElementById('weather');
 
 // Get location
 var functions = {
@@ -17,11 +17,11 @@ var functions = {
     var pathWithCurrentLocal = 'https://weather.millergeek.xyz/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=31405be1fb8f6e7f0bb74137ccb08125';
 
     ajaxRequest(pathWithCurrentLocal, function(data){
-      var getTemp = data.main.temp;
-      var getCity = data.name;
-      var getWeather = data.weather[0].description;
       var lat = data.coord.lat;
       var lon = data.coord.lon;
+      var getTemp = data.main.temp;
+      var getCity = data.name;
+      getWeather = data.weather[0].description;
 
       // Converting Weather from Kelvin to Fahrenheit and Celsius
       var fahrenheitConverter = Math.round((getTemp * (9/5) - 459.67) * 100) / 100;
@@ -29,11 +29,9 @@ var functions = {
       fahrenheit = fahrenheitConverter + " fahrenheit";
       celsius = celsiusConverter + " celsius";
 
-
-
-      city[0].innerHTML = getCity;
-      temp[0].innerHTML = fahrenheit;
-      weather[0].innerHTML = getWeather;
+      city.innerHTML = getCity;
+      temp.innerHTML = fahrenheit;
+      weather.innerHTML = getWeather;
 
       console.log(data);
       console.log(getTemp);
@@ -41,6 +39,8 @@ var functions = {
       console.log(getWeather);
       console.log("Lat: " + lat);
       console.log("Lon: " + lon);
+
+      functions.getWeatherIcon();
     });
   },
   toggleTemp: function() {
@@ -50,6 +50,44 @@ var functions = {
     } else {
       currentTempType.innerHTML = fahrenheit;
     }
+  },
+  getWeatherIcon: function() {
+    var weatherIcon = document.getElementById('weatherIcon');
+    var currentWeather = weather.innerHTML;
+    var image = '';
+
+    console.log(weather);
+    console.log(currentWeather);
+
+    switch(currentWeather) {
+      case "sunny":
+      case "clear sky":
+        image = 'img/weather-clear.png';
+        break;
+      case "few clouds":
+        image = 'img/weather-few-clouds.png';
+        break;
+      case "rainy":
+        image = 'img/weather-showers-night.png';
+        break;
+      case "cloudy":
+        image = 'img/weather-clouds.png';
+        break;
+      case "light rain":
+        image = 'img/weather-rain-night.png';
+        break;
+      case "stormy":
+      case "thunderstorm":
+        image = 'img/weather-storm-night.png';
+        break;
+      case "broken clouds":
+        image = 'img/weather-few-clouds-night.png';
+        break;
+      default:
+        image = 'img/weather-none-available.png';
+        console.log("I can't seem to find the weather.");
+    }
+  weatherIcon.src = image;
   }
 };
 
@@ -75,6 +113,35 @@ function ajaxRequest (path, callback) {
 }
 
 functions.getLocation();
+
+// function getWeatherIcon(currentWeather) {
+//   var weatherIcon = document.getElementById('weatherIcon');
+//
+//   switch(currentWeather) {
+//     case "sunny":
+//       weatherIcon.src = 'img/weather-clear.png';
+//       break;
+//     case "rainy":
+//       weatherIcon.src = 'img/weather-showers-night.png';
+//       break;
+//     case "cloudy":
+//       weatherIcon.src = 'img/weather-clouds.png';
+//       break;
+//     case "light rain":
+//       weatherIcon.src = 'img/weather-rain-night.png';
+//       break;
+//     case "stormy":
+//     case "thunderstorm":
+//       weatherIcon.src = 'img/weather-storm-night.png';
+//       break;
+//     case "broken clouds":
+//       weatherIcon.src = 'img/weather-few-clouds-night.png';
+//       break;
+//     default:
+//       console.log("I can't seem to find the weather.");
+//   }
+// }
+
 
 
 
